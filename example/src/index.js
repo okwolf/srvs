c5a1e984e0b625a7c69335eefeee547d4cb654ea
Wolfgang <wolf@okwolf.com>
2017-11-09 19:49:30 -0800

    simplifying example

--- example/src/index.js
+++ example/src/index.js
@@ -1,2 +1,9 @@
-import app from "./app";
-app();
+import { app } from "hyperapp";
+import H1 from "./components/H1";
+
+app({
+  state: {
+    message: "hello world"
+  },
+  view: ({ message }) => H1({}, message)
+});
