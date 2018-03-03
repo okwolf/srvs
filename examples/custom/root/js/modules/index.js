99a73bccd525ac777443c3d90ba06c9a62fc07b0
Wolfgang <wolf@okwolf.com>
2018-03-02 21:26:41 -0800

    updating resolved unpkg url

--- examples/custom/root/js/modules/index.js
+++ examples/custom/root/js/modules/index.js
@@ -1,4 +1,3 @@
 import { state, view } from "./main";
 
-const actions = {};
-export default { state, actions, view };
+export default { state, view };
--- src/server/streamPath.js
+++ src/server/streamPath.js
@@ -24,7 +24,7 @@ const rewriteScript = (contextPath, withVersion) =>
           `${imports} "${
             module.startsWith(".")
               ? `${contextPath}/${module}`
-              : `https://unpkg.com/${withVersion(module)}?main=module`
+              : `https://unpkg.com/${withVersion(module)}?module`
           }"`
       )
       .replace(
